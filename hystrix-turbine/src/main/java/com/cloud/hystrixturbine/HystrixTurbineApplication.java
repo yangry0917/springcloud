cdc9c5abf2bf081a3299cfb0010b9275f4b013a6
yangry0917 <1021794070@qq.com>
2020-11-24 09:19:14 +0800

    turbine功能添加

---
+++ hystrix-turbine/src/main/java/com/cloud/hystrixturbine/HystrixTurbineApplication.java
@@ -0,0 +1,42 @@
+package com.cloud.hystrixturbine;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+ //和该注解@EnableEurekaClient功能一样 //开启eureka客户端功能
+@EnableTurbine
+public class HystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixTurbineApplication.class, args);
+    }
+    /**
+     * hystrix dashboard配置内容
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
