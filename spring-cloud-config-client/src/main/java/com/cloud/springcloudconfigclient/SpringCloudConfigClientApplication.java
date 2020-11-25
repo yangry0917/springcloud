8f3e7a22f0567de2efbffc85263c987f82f4a27d
yangry0917 <1021794070@qq.com>
2020-11-25 17:12:06 +0800

    配置中心服务端、客户端配置完成

--- spring-cloud-config-client/src/main/java/com/cloud/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/cloud/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -1,19 +1,22 @@
-package com.cloud.springcloudconfigserver;
+package com.cloud.springcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@RestController
+@EnableDiscoveryClient
 @EnableEurekaClient //在服务启动后自动注册到Eureka中！
-@EnableDiscoveryClient //服务发现~
-@EnableConfigServer
-public class SpringCloudConfigServerApplication {
+//@EnableDiscoveryClient //服务发现~
+public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
     }
 
 }
---
+++ spring-cloud-config-client/src/main/java/com/cloud/springcloudconfigclient/config/DemoConfiguration.java
@@ -0,0 +1,14 @@
+package com.cloud.springcloudconfigclient.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@EnableAutoConfiguration
+public class DemoConfiguration {
+
+   /* @Value("${neo.hello}")
+    private String hello;*/
+}
